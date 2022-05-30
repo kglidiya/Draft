1921cdf5e37054b6d8f9f472d856a20838a25850
kglidiya <betysmith@yandex.ru>
2022-05-30 10:33:54 +0300

    js fix

--- script.js
+++ script.js
@@ -5,24 +5,25 @@ const overlay = document.querySelector('.overlay');
 
 test.addEventListener('click', function() {
  background.classList.add('video_notes');
+ overlay.classList.add('overlay_notes');
    if(background.classList.contains('video_keys')) {
      background.classList.remove('video_keys');
 }
-     if(background.classList.contains('overlay_keys')) {
-     background.classList.remove('overlay_keys');
+     if(overlay.classList.contains('overlay_keys')) {
+        overlay.classList.remove('overlay_keys');
 }
- background.classList.add('overlay_notes');
+
 })
 
 test2.addEventListener('click', function() {
     background.classList.add('video_keys');
+    overlay.classList.add('overlay_keys');
    if(background.classList.contains('video_notes')) {
      background.classList.remove('video_notes');
 }
   
-    overlay.classList.add('overlay_keys');
 
-  if(background.classList.contains('overlay_notes')) {
+  if(overlay.classList.contains('overlay_notes')) {
      overlay.classList.remove('overlay_notes');
 }
 
@@ -49,90 +50,4 @@ window.addEventListener('scroll', function () {
         
     })
 
-    let sliderPosition = 0; // начальная позиция дорожки
-    const slidesToShow = 1;
-    const slidesToScroll = 2;
-    const sliderContainer = document.querySelector('.slider-wrapper')
-    const sliderTrack = document.querySelector('.slider-track')
-    const sliderItem = document.querySelector('.slider-item')
-    const allItems = document.querySelectorAll('.slider-item')
-    const itemCount = allItems.length
-    const sliderItemWidth = sliderTrack.clientWidth / slidesToShow;
-    const sliderContainerWidth = sliderContainer.width;
-    const movePosition = slidesToScroll * sliderItemWidth;
-    const popup = document.querySelector('.popup');
-    const popupCloseBtn = document.querySelector('.popup__button');
-    const popupImage = document.querySelector('.popup__image')
-   
-    // ширина дорожки определяется как разница между шириной всех картинок и шириной контейнера
-    // разница нужна для того, чтобы прокрутка не проводилась дальше последнего фото
-   // const sliderTrackWidth = allItems.length * sliderItemWidth - sliderContainerWidth; 
-    //console.log(sliderTrackWidth)
-   const sliderButtonPrev = document.querySelector('.prev')
-   const sliderButtonNext = document.querySelector('.next')
-
- 
-    allItems.forEach(function (item) {
     
-       item.style.width = `${sliderItemWidth}px`;
-    }) 
-
-  
-
-    
-
-sliderButtonNext.addEventListener('click', function(){
-   sliderPosition -= movePosition;
-    sliderTrack.style.transform = `translateX(${sliderPosition}px`;
-    
-    checkBtn()
-});
-
-sliderButtonPrev.addEventListener('click', function(){
-    sliderPosition += movePosition;
-    sliderTrack.style.transform = `translateX(${sliderPosition}px`;
-   checkBtn()
- 
-});
-
-const checkBtn = function() {
- 
-    if (sliderPosition === 0) {
-        sliderButtonPrev.disabled = true;
-    } else if (sliderPosition < 0) {
-        sliderButtonPrev.disabled = false;
-    }
-
-   if(sliderPosition === -((itemCount - slidesToShow) * sliderItemWidth)) {
-        sliderButtonNext.disabled = true;
-    } else {
-        sliderButtonNext.disabled = false;
-    }
-    
-}
-
-checkBtn()
-//console.log(sliderPosition)
-
-function openPopup(e) {
-    popup.classList.add('popup_opended')
-    popupImage.src = e.target.src
- 
-
-}
-
-function closePopup() {
-    popup.classList.remove('popup_opended')
-    
-}
-
-allItems.forEach(function (item) {
-    
-    item.addEventListener('click', openPopup)
-   // console.log(item.closest('image'))
-  
-    
-})
-
-popupCloseBtn.addEventListener('click', closePopup)
-
